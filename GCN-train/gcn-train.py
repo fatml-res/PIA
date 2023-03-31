2f1173cfc9d3ece19746f8842e8748618ef97251
xiulingwang33 <84334716+xiulingwang33@users.noreply.github.com>
2023-03-31 12:30:01 -0400

    Update gcn-train.py

--- GCN-train/gcn-train.py
+++ GCN-train/gcn-train.py
@@ -99,13 +99,13 @@ parser.add_argument('--no-cuda', action='store_true', default=True,
 parser.add_argument('--fastmode', action='store_true', default=False,
                     help='Validate during training pass.')
 parser.add_argument('--seed', type=int, default=42, help='Random seed.')
-parser.add_argument('--epochs', type=int, default=2000,
+parser.add_argument('--epochs', type=int, default=5000,
                     help='Number of epochs to train.')
 parser.add_argument('--lr', type=float, default=0.01,
                     help='Initial learning rate.')
 parser.add_argument('--weight_decay', type=float, default=5e-4,
                     help='Weight decay (L2 loss on parameters).')
-parser.add_argument('--hidden', type=int, default=64,
+parser.add_argument('--hidden', type=int, default=128,
                     help='Number of hidden units.')
 parser.add_argument('--dropout', type=float, default=0,
                     help='Dropout rate (1 - keep probability).')
